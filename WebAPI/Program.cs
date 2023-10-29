e7b2ad6991c1127c540059420537bf271d9689ee
Reynaldo <rodriguezreynaldo1698@outlook.com>
2023-10-29 12:49:17 -0600

    playing with authorization

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Persistence;
@@ -7,8 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthentication()
+       .AddBearerToken(IdentityConstants.BearerScheme);
 builder.Services.AddAuthorizationBuilder();
+
 builder.Services.AddIdentityCore<MyUser>()
        .AddEntityFrameworkStores<ApplicationContext>()
        .AddApiEndpoints();
@@ -21,7 +24,15 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/Fake/Authenticated", () => "Hello World!").RequireAuthorization();
+app.MapGet("/Fake/AuthenticatedWithClaims", 
+           (ClaimsPrincipal user) => $@"Fuck you {user.Identity?.Name} - You are 
+                                               Authenticated with ${user.Identity?.AuthenticationType}
+                                               Your claims are {string.Join("-", user.Claims)}
+                                               Your Identity are {string.Join("-", user.Identities.Select(x => x))}")
+    .RequireAuthorization();
+app.MapGet("/Fake/NoAuthenticated", () => "Hello World!");
+app.MapGet("/Fake/Anonymous", () => "Hello World!").AllowAnonymous();
 
 app.MapIdentityApi<MyUser>();
 
